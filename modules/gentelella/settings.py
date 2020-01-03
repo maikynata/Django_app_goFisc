edf7ae5b2446ac8f03805be6868e97c617ddbbf9
Maiky Nata <maikynata25@gmail.com>
2020-01-03 11:45:18 -0800

    Configuring heroky django-app

--- modules/gentelella/settings.py
+++ modules/gentelella/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 from decouple import config
 from dj_database_url import parse as dburl
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,4 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
--- modules/gentelella/wsgi.py
+++ modules/gentelella/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gentelella.settings")
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = Cling(get_wsgi_application())
