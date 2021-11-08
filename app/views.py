0ff5f243f3e302f62393efd879ea40d7e7cc1d2c
maikynata <maikynata25@gmail.com>
2021-11-08 16:56:15 -0300

    view working import the CSV

--- app/views.py
+++ app/views.py
@@ -29,6 +29,51 @@ def gentella_html(request):
     template = loader.get_template('app/' + load_template)
     return HttpResponse(template.render(context, request))
 
+
+# def form_upload(request):
+# 	data = {}
+# 	if "GET" == request.method:
+# 		return render(request, "app/form_upload.html", data)
+#     # if not GET, then proceed
+# 	try:
+# 		csv_file = request.FILES["csv_file"]
+# 		if not csv_file.name.endswith('.csv'):
+# 			messages.error(request,'File is not CSV type')
+# 			return HttpResponseRedirect(reverse("app:form_upload"))
+#         #if file is too large, return
+# 		if csv_file.multiple_chunks():
+# 			messages.error(request,"Uploaded file is too big (%.2f MB)." % (csv_file.size/(1000*1000),))
+# 			return HttpResponseRedirect(reverse("app:form_upload"))
+
+# 		file_data = csv_file.read().decode("utf-8")		
+
+# 		lines = file_data.split("\n")
+# 		#loop over the lines and save them in db. If error , store as string and then display
+# 		for line in lines:						
+# 			fields = line.split(",")
+# 			data_dict = {}
+# 			data_dict["id"] = fields[0]
+# 			data_dict["tipo"] = fields[1]
+# 			data_dict["versao"] = fields[2]
+# 			data_dict["listaufs"] = fields[3]
+# 			data_dict["hash"] = fields[4]
+# 			try:
+# 				form = EventsForm(data_dict)
+# 				if form.is_valid():
+# 					form.save()					
+# 				else:
+# 					logging.getLogger("error_logger").error(form.errors.as_json())												
+# 			except Exception as e:
+# 				logging.getLogger("error_logger").error(repr(e))					
+# 				pass
+
+# 	except Exception as e:
+# 		logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
+# 		messages.error(request,"Unable to upload file. "+repr(e))
+
+# 	return HttpResponseRedirect(reverse("app:form_upload"))
+
+
 # one parameter named request
 def form_upload(request):
     template = "form_upload.html"
@@ -38,12 +83,12 @@ def form_upload(request):
     prompt = {
         'order': 'Order of the CSV should be name, email, address, phone, profile',
         'tabelas': data    
-              }
+    }
 
     # GET request returns the value of the data with the specified key.
     if request.method == "GET":
         return render(request, template, prompt)
-    csv_file = request.FILES['file']
+    csv_file = request.FILES['csv_file']
 
     # let's check if it is a csv file
     if not csv_file.name.endswith('.csv'):
